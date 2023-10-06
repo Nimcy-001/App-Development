be66a0e0a3ce468bf16456d6a8ceac08b69fe9f7
Nimcy-001 <107349302+Nimcy-001@users.noreply.github.com>
2023-10-06 15:08:59 +0530

    Add files via upload

---
+++ Day 12/Amity/src/main/java/com/project/Amity/Controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.project.Amity.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.Amity.Service.AuthService;
+import com.project.Amity.constant.Api;
+import com.project.Amity.dto.request.AuthenticationRequest;
+import com.project.Amity.dto.request.RegisterRequest;
+import com.project.Amity.dto.response.AuthenticationResponse;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(Api.AUTH)
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+	@Autowired
+    AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        boolean isRegistered = authService.userRegistration(request);
+        return isRegistered ? ResponseEntity.ok("User registered successfully")
+                : ResponseEntity.badRequest().body("Sommething went wrong!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authService.userAuthentication(request));
+    }
+}
---
+++ Day 12/Amity/src/main/java/com/project/Amity/Controller/SignupController.java
@@ -0,0 +1,10 @@
+package com.project.Amity.Controller;
+
+public class SignupController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
