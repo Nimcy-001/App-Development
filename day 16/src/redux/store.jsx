316b940ef50c1ee9b2727e8bb306e971021bc0ba
Nimcy-001 <107349302+Nimcy-001@users.noreply.github.com>
2023-10-13 11:10:44 +0530

    Delete day 16/src directory

--- day 16/src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './userSlice'
-import cartReducer from './cartSlice'
-export default configureStore({
-    reducer :{
-        user:userReducer,
-        cart: cartReducer,
-    }
-})
